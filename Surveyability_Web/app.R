e649fc3094a9878af83e20d6ecbb8b3795ec5faf
akmcclary <akmcclary@gmail.com>
2016-12-14 08:16:22 -0800

    Updates
    Minor tweaks and some commented out but potentially useful code

--- Surveyability_Web/app.R
+++ Surveyability_Web/app.R
@@ -5,32 +5,41 @@ library("plotly")
 library("ggplot2")
 library("magrittr")
 library("lubridate")
-library("xlsx")
 library("DT")
-library("splitstackshape")
 
+library("readr")
 #flowData<- read.xlsx("Daily_Precip_Discharge_Monitoring.xlsx", sheetName = "DailyGauge")
-#tripData<-read.xlsx("TripData.xlsx", sheetName = "TripDataFish")
+#tripData<-read.xlsx("TripDataFish.xlsx", sheetName = "AllData")
 
-flowData<- read.csv("Daily_Precip_Discharge_Monitoring.csv")
-tripData<-read.csv("TripDataFish.csv")
+#Import Data
+flowData<- read_csv("Daily_Precip_Discharge_Monitoring.csv")
+tripData<-read_csv("TripDataFish.csv")
+#fish<-read.csv("Fish.csv")
+#redds<-read.csv("Redds.csv")
 
-
-tripData$DATE<-mdy(tripData$Date)
+#Convert Dates to Dates in R
+tripData$DATE<-tripData$Date
 flowData$DATE<-mdy(flowData$DATE)
+#fish$Date<-mdy(fish$Date)
+#redds$Date<-mdy(redds$Date)
+
+#FishTotals<-aggregate(fish, by= list(ReachName = fish$ReachName, Tributary = fish$Tributary, Season = fish$Season, Species = fish$Species), FUN = length)
+#redds<-filter(redds,ReddAge==1)
+#ReddTotals<-aggregate(redds, by= list(ReachName = redds$ReachName, Tributary = redds$Tributary, Season = redds$Season, Species = redds$Species), FUN = length)
+
 gaugeNames<-c(colnames(flowData))
 gaugeNames<-gaugeNames[2:14]
 flowData[gaugeNames]<- sapply(flowData[gaugeNames],as.numeric)
 mergedData<- join(flowData, tripData, by = "DATE", "inner")
 mergedData$Fishing<-as.factor(mergedData$Fishing)
 
-
+#ReddTotalsPlot<-redds %>% filter(ReachName=="RED 1") %>% filter(Tributary == "REDWOOD CREEK") %>% filter(Season == "2015-2016") %>% ggplot(aes(x=Species, fill=Species))+ geom_bar()
 #Get the most recent date the trip was surveyed if it was surveyed this season
 mostRecentTrip <- tripData %>% filter(Season == "2016-2017")%>% filter(Fishing == 1) %>% group_by(ReachName, Tributary) %>% filter(DATE == max(DATE)) %>% arrange(ReachName,Tributary)
 mostRecentTrip$daysSinceSurveyed<- (today("America/Los_Angeles"))-mostRecentTrip$DATE
 TripCounts<-tripData %>% filter(Season == "2016-2017")%>% filter(Fishing == 1) %>%count_(c("ReachName", "Tributary"))
 mostRecentTrip<-inner_join(TripCounts, mostRecentTrip, by = c("ReachName", "Tributary"))
-
+#ZacTrips<-tripData %>% filter(Season == "2016-2017")%>% filter(Fishing == 1) %>%filter(grepl('ZR', Crew))
 #FilteredData<- mergedData %>% filter(REACHNAME == selectedReach)
 #maxSurveyedCFS<- max(FilteredData[selectedGauge])
 
@@ -38,7 +47,7 @@ mostRecentTrip<-inner_join(TripCounts, mostRecentTrip, by = c("ReachName", "Trib
 #GreenValleyPlot + geom_line(data = flowData, aes(DATE, MIL.School..ft..))
 
 surveyedReaches<-unique(tripData$ReachName)
-surveyedReaches<- as.vector.factor(surveyedReaches)
+#surveyedReaches<- as.vector.factor(surveyedReaches)
 surveyedReaches<-sort(surveyedReaches)
 
 gaugeList <- colnames(flowData)
@@ -57,11 +66,11 @@ ui<- fluidPage(
                   surveyedReaches), selectInput("Gauge", "Gauge:", gaugeList),
       dateInput('startDate',
                 label = 'Start Date: yyyy-mm-dd',
-                value = '2016-1-1'
+                value = '2016-01-1'
       ),
       dateInput('enddate',
-                label = 'End input: yyyy-mm-dd',
-                value = '2016-11-30'
+                label = 'End Date: yyyy-mm-dd',
+                value = '2016-12-07'
       ), width = 2
     ),
     
@@ -89,12 +98,12 @@ server<- function(input, output) {
     
     FishData<- mergedData %>%filter(ReachName == input$Reach)%>%filter(DATE >= input$startDate & DATE <= input$enddate)
     FishData<- FishData[,c("Date","ReachName", "Tributary", "Crew", "CohoIndividuals","SteelheadIndividuals","ChinookIndividuals", "SalmonidSpIndividuals", "CohoRedds", "SteelheadRedds", "ChinookRedds", "SalmonidSpRedds","Comments")]
-    DT::datatable(FishData, colnames = c("Date","Reach", "Tributary", "Crew", "Coho","Steelhead","Chinook", "SalmonidSp", "Coho Redds", "Steelhead Redds", "Chinook Redds", "SalmonidSp Redds","Comments"), options = list(pagelength = 10, autoWidth = FALSE))
+    DT::datatable(FishData, colnames = c("Date","Reach", "Tributary", "Crew", "Coho","Steelhead","Chinook", "SalmonidSp", "Coho Redds", "Steelhead Redds", "Chinook Redds", "SalmonidSp Redds","Comments"), rownames = FALSE, options = list(order = list(list(0, 'desc')), pagelength = 10, autoWidth = FALSE))
   })
   
   output$mostRecentSurveyTable = renderDataTable({
     mostRecentTrip<- mostRecentTrip[,c("daysSinceSurveyed", "ReachName", "Tributary", "Date", "n" )]
-    DT::datatable(mostRecentTrip, colnames = c("Days Since Surveyed", "Reach", "Tributary", "Date Last Surveyed", "Number of Times Surveyed this Year" ), rownames = FALSE, options = list(pageLength = 55, order = list(list(0, 'desc'))))
+    DT::datatable(mostRecentTrip, colnames = c("Days Since Surveyed", "Reach", "Tributary", "Date Last Surveyed", "Number of Times Surveyed this Year" ), rownames = FALSE, options = list(pageLength = 57, order = list(list(0, 'desc'))))
   })
   # output$results<- renderTable({
   #   FilteredData<- filter(mergedData, REACHNAME == input$Reach)

292e2fa4deee243e4bd078aab68b190a46866296
akmcclary <akmcclary@gmail.com>
2016-12-20 15:56:00 -0800

    uploaded data

--- Surveyability_Web/ConvertFishDatatoCSV.R
+++ Surveyability_Web/ConvertFishDatatoCSV.R
@@ -1,3 +1,7 @@
 library("xlsx")
-tripData<-read.xlsx("TripDataFish.xlsx", sheetName = "AllData")
+tripData<-read.xlsx("TripData.xlsx", sheetName = "TripDataFish")
 write.csv(tripData,file="TripDataFish.csv")
+tripData<-read.xlsx("TripData.xlsx", sheetName = "Individuals")
+write.csv(tripData,file="Fish.csv")
+tripData<-read.xlsx("TripData.xlsx", sheetName = "Redds")
+write.csv(tripData,file="Redds.csv")

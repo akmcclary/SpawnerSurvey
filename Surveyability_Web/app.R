52c056c05957b9d455306e0cce7d21e9eeb5b122
akmcclary <akmcclary@gmail.com>
2017-04-04 15:21:02 -0700

    data updated

--- Surveyability_Web/app.R
+++ Surveyability_Web/app.R
@@ -71,8 +71,8 @@ mostRecentTrip<-inner_join(TripCounts, mostRecentTrip, by = c("ReachName", "Trib
 
 #GreenValleyPlot <- tripData %>% filter(REACHNAME == selectedReach)%>% ggplot(aes(x=DATE, y = FISHING)) + geom_point()+ scale_x_date()
 #GreenValleyPlot + geom_line(data = flowData, aes(DATE, MIL.School..ft..))
-CrewNames<-c("ZR","WB","CO","MT","LE","AMJ","NB","AB","AM ","AI", "SB", "JP", "KS","RA", "BA", "JRR", "AJ")
-CrewCounts<-data.frame(FishTotals=numeric(17), ReddTotals=numeric(17), CombinedTotals=numeric(17), NumberOfSurveys=numeric(17), FishPerSurvey=numeric(17), ReddsPerSurvey= numeric(17), Efficiency = numeric(17),KilometersSurveyed = numeric(17),  FishPerkm=numeric(17), ReddsPerkm= numeric(17), SpatialEfficiency = numeric(17) )
+CrewNames<-c("ZR","WB","CO","MT","LE","AMJ","NB","AB","AM ","AI", "SB", "JP", "KS","RA", "BA", "JRR", "AJ", "GH", "TS", "CE")
+CrewCounts<-data.frame(FishTotals=numeric(20), ReddTotals=numeric(20), CombinedTotals=numeric(20), NumberOfSurveys=numeric(20), FishPerSurvey=numeric(20), ReddsPerSurvey= numeric(20), Efficiency = numeric(20),KilometersSurveyed = numeric(20),  FishPerkm=numeric(20), ReddsPerkm= numeric(20), SpatialEfficiency = numeric(20) )
 surveyedReaches<-unique(tripData$ReachName)
 surveyedTributaries<-unique(tripData$Tributary)
 #surveyedReaches<- as.vector.factor(surveyedReaches)
@@ -463,7 +463,7 @@ server<- function(input, output) {
       }
       CrewCounts$CombinedTotals<-CrewCounts$FishTotals + CrewCounts$ReddTotals
       
-      DT::datatable(CrewCounts, colnames = c("Crew", "Fish Total", "Redd Total", "Combined Total"), rownames = FALSE, options = list( pageLength= 17, order=list(3,'desc')))
+      DT::datatable(CrewCounts, colnames = c("Crew", "Fish Total", "Redd Total", "Combined Total"), rownames = FALSE, options = list( pageLength= 20, order=list(3,'desc')))
     } else for (name in CrewNames){
       CrewCounts$CrewName[i] = name
       CrewCounts$FishTotals[i]<-fish %>% filter(Season == "2016-2017")%>%filter(grepl(name, Crew))%>%filter(Species == input$SpeciesSelected)%>%nrow()
@@ -490,7 +490,7 @@ server<- function(input, output) {
     CrewCounts$SpatialEfficiency<-round(CrewCounts$SpatialEfficiency, 4)
     CrewCounts<-CrewCounts[,c(12,1,2,3,4,5,6,7,8,9,10,11)]
     
-    DT::datatable(CrewCounts, colnames = c("Crew", "Fish Total", "Redd Total", "Combined Total", "Number of Surveys", "Fish/Survey", "Redds/Survey", "Survey Efficiency","Total Km Surveyed","Fish/Km", "Redds/Km", "Spatial Efficiency" ), rownames = FALSE, options = list(pageLength= 17,  order=list(3,'desc')))
+    DT::datatable(CrewCounts, colnames = c("Crew", "Fish Total", "Redd Total", "Combined Total", "Number of Surveys", "Fish/Survey", "Redds/Survey", "Survey Efficiency","Total Km Surveyed","Fish/Km", "Redds/Km", "Spatial Efficiency" ), rownames = FALSE, options = list(pageLength= 20,  order=list(5,'desc')))
   })
   
 }
